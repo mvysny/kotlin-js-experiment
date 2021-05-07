798f9f60f4c6ac776aa6565c16e2d500df389429
Martin Vysny <mavi@vaadin.com>
2021-05-07 13:57:58 +0300

    v bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("org.jetbrains.kotlin.js") version "1.4.32"
-    kotlin("plugin.serialization") version "1.4.32"
+    id("org.jetbrains.kotlin.js") version "1.5.0"
+    kotlin("plugin.serialization") version "1.5.0"
 }
 
 defaultTasks("clean", "build")
@@ -15,7 +15,7 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib-js"))
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-js:1.4.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.0")
 }
 
 kotlin {
